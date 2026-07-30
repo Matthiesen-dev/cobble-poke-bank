43228b1c3bf9d9de68bd516ef0486c93ab5fd26a
Adam Matthiesen <amatthiesen@outlook.com>
2026-07-29 21:06:32 -0700

    Refactor Pokemon utility methods to improve move and stat display

--- common/src/main/java/dev/matthiesen/cobble_poke_bank/common/utility/PokemonUtility.java
+++ common/src/main/java/dev/matthiesen/cobble_poke_bank/common/utility/PokemonUtility.java
@@ -4,6 +4,7 @@ import com.cobblemon.mod.common.api.pokemon.stats.Stats;
 import com.cobblemon.mod.common.item.PokemonItem;
 import com.cobblemon.mod.common.pokemon.Gender;
 import com.cobblemon.mod.common.pokemon.Pokemon;
+import com.cobblemon.mod.common.pokemon.PokemonStats;
 import com.cobblemon.mod.common.util.LocalizationUtilsKt;
 import com.google.gson.JsonObject;
 import dev.matthiesen.matthiesen_core.common.utility.item.ItemBuilder;
@@ -14,6 +15,8 @@ import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.item.ItemStack;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public final class PokemonUtility {
@@ -68,7 +71,9 @@ public final class PokemonUtility {
         };
     }
 
-    private static Component[] loreBuilder(Pokemon pokemon) {
+    private record PokemonMoves(String moveOne, String moveTwo, String moveThree, String moveFour) {}
+
+    private static PokemonMoves getMoves(Pokemon pokemon) {
         String moveOne = !pokemon.getMoveSet().getMoves().isEmpty() ?
                 Objects.requireNonNull(pokemon.getMoveSet().get(0)).getDisplayName().getString() : "Empty";
         String moveTwo = pokemon.getMoveSet().getMoves().size() >= 2 ?
@@ -77,70 +82,119 @@ public final class PokemonUtility {
                 Objects.requireNonNull(pokemon.getMoveSet().get(2)).getDisplayName().getString() : "Empty";
         String moveFour = pokemon.getMoveSet().getMoves().size() >= 4 ?
                 Objects.requireNonNull(pokemon.getMoveSet().get(3)).getDisplayName().getString() : "Empty";
+        return new PokemonMoves(moveOne, moveTwo, moveThree, moveFour);
+    }
 
-        return new Component[]{
-                Component.literal(pokemon.getCaughtBall().item().getDefaultInstance().getDisplayName().getString())
-                        .setStyle(Style.EMPTY.withItalic(true).withColor(ChatFormatting.DARK_GRAY)),
-                Component.literal("Level: ").withStyle(ChatFormatting.AQUA)
-                        .append(Component.literal(String.valueOf(pokemon.getLevel())).withStyle(ChatFormatting.WHITE)),
-                Component.literal("Nickname: ").withStyle(ChatFormatting.DARK_GREEN)
-                        .append(Component.literal(
-                        pokemon.getNickname() != null ? pokemon.getNickname().getString() : "No nickname"
-                ).withStyle(ChatFormatting.WHITE)),
-                Component.literal("Held Item: ").withStyle(ChatFormatting.DARK_PURPLE)
-                        .append(Component.literal(
-                        pokemon.heldItem().isEmpty() ? "No held item" : pokemon.heldItem().getDisplayName().getString()
-                ).withStyle(ChatFormatting.WHITE)),
-                Component.literal("Nature: ").withStyle(ChatFormatting.YELLOW)
-                        .append(LocalizationUtilsKt.lang(pokemon.getNature().getDisplayName().replace("cobblemon.", ""))
-                        .withStyle(ChatFormatting.WHITE)),
-                Component.literal("Ability: ").withStyle(ChatFormatting.GOLD)
-                        .append(LocalizationUtilsKt.lang(pokemon.getAbility().getDisplayName().replace("cobblemon.", ""))
-                        .withStyle(ChatFormatting.WHITE)),
-                Component.literal("IVs: ").withStyle(ChatFormatting.LIGHT_PURPLE),
-                Component.literal("  HP: ").withStyle(ChatFormatting.RED)
-                        .append(Component.literal(String.valueOf(pokemon.getIvs().getOrDefault(Stats.HP)))
-                                .withStyle(ChatFormatting.WHITE))
-                        .append(Component.literal("  Atk: ").withStyle(ChatFormatting.BLUE)
-                                .append(Component.literal(String.valueOf(pokemon.getIvs().getOrDefault(Stats.ATTACK)))
-                                        .withStyle(ChatFormatting.WHITE)))
-                        .append(Component.literal("  Def: ").withStyle(ChatFormatting.GRAY)
-                        .append(Component.literal(String.valueOf(pokemon.getIvs().getOrDefault(Stats.DEFENCE)))
-                                .withStyle(ChatFormatting.WHITE))),
-                Component.literal("  SpAtk: ").withStyle(ChatFormatting.AQUA)
-                        .append(Component.literal(String.valueOf(pokemon.getIvs().getOrDefault(Stats.SPECIAL_ATTACK)))
-                                .withStyle(ChatFormatting.WHITE))
-                        .append(Component.literal("  SpDef: ").withStyle(ChatFormatting.YELLOW)
-                                .append(Component.literal(String.valueOf(pokemon.getIvs().getOrDefault(Stats.SPECIAL_DEFENCE)))
-                                        .withStyle(ChatFormatting.WHITE)))
-                        .append(Component.literal("  Spd: ").withStyle(ChatFormatting.GREEN)
-                        .append(Component.literal(String.valueOf(pokemon.getIvs().getOrDefault(Stats.SPEED)))
-                                .withStyle(ChatFormatting.WHITE))),
-                Component.literal("EVs: ").withStyle(ChatFormatting.DARK_AQUA),
-                Component.literal("  HP: ").withStyle(ChatFormatting.RED)
-                        .append(Component.literal(String.valueOf(pokemon.getEvs().getOrDefault(Stats.HP)))
-                                .withStyle(ChatFormatting.WHITE))
-                        .append(Component.literal("  Atk: ").withStyle(ChatFormatting.BLUE)
-                                .append(Component.literal(String.valueOf(pokemon.getEvs().getOrDefault(Stats.ATTACK)))
-                                        .withStyle(ChatFormatting.WHITE)))
-                        .append(Component.literal("  Def: ").withStyle(ChatFormatting.GRAY)
-                        .append(Component.literal(String.valueOf(pokemon.getEvs().getOrDefault(Stats.DEFENCE)))
-                                .withStyle(ChatFormatting.WHITE))),
-                Component.literal("  SpAtk: ").withStyle(ChatFormatting.AQUA)
-                        .append(Component.literal(String.valueOf(pokemon.getEvs().getOrDefault(Stats.SPECIAL_ATTACK)))
-                                .withStyle(ChatFormatting.WHITE))
-                        .append(Component.literal("  SpDef: ").withStyle(ChatFormatting.YELLOW)
-                                .append(Component.literal(String.valueOf(pokemon.getEvs().getOrDefault(Stats.SPECIAL_DEFENCE)))
-                                        .withStyle(ChatFormatting.WHITE)))
-                        .append(Component.literal("  Spd: ").withStyle(ChatFormatting.GREEN)
-                        .append(Component.literal(String.valueOf(pokemon.getEvs().getOrDefault(Stats.SPEED)))
-                                .withStyle(ChatFormatting.WHITE))),
+    private static List<MutableComponent> getMovesComponents(Pokemon pokemon) {
+        PokemonMoves moves = getMoves(pokemon);
+        return List.of(
                 Component.literal("Moves: ").withStyle(ChatFormatting.DARK_GREEN),
-                Component.literal(" ").append(Component.literal(moveOne).withStyle(ChatFormatting.WHITE)),
-                Component.literal(" ").append(Component.literal(moveTwo).withStyle(ChatFormatting.WHITE)),
-                Component.literal(" ").append(Component.literal(moveThree).withStyle(ChatFormatting.WHITE)),
-                Component.literal(" ").append(Component.literal(moveFour).withStyle(ChatFormatting.WHITE)),
-                Component.literal("Form: ").withStyle(ChatFormatting.GOLD).append(pokemon.getForm().getName())
+                Component.literal(" ").append(Component.literal(moves.moveOne()).withStyle(ChatFormatting.WHITE)),
+                Component.literal(" ").append(Component.literal(moves.moveTwo()).withStyle(ChatFormatting.WHITE)),
+                Component.literal(" ").append(Component.literal(moves.moveThree()).withStyle(ChatFormatting.WHITE)),
+                Component.literal(" ").append(Component.literal(moves.moveFour()).withStyle(ChatFormatting.WHITE))
+        );
+    }
+
+    private static ChatFormatting getStatColor(Stats stat) {
+        return switch (stat) {
+            case HP -> ChatFormatting.RED;
+            case ATTACK -> ChatFormatting.BLUE;
+            case DEFENCE -> ChatFormatting.GRAY;
+            case SPECIAL_ATTACK -> ChatFormatting.AQUA;
+            case SPECIAL_DEFENCE -> ChatFormatting.YELLOW;
+            case SPEED -> ChatFormatting.GREEN;
+            case EVASION -> ChatFormatting.DARK_GREEN;
+            case ACCURACY -> ChatFormatting.DARK_RED;
         };
+    }
+
+    private static String getStatLabel(Stats stat) {
+        return switch (stat) {
+            case HP -> "HP";
+            case ATTACK -> "Atk";
+            case DEFENCE -> "Def";
+            case SPECIAL_ATTACK -> "SpAtk";
+            case SPECIAL_DEFENCE -> "SpDef";
+            case SPEED -> "Spd";
+            case EVASION -> "Evasion";
+            case ACCURACY -> "Accuracy";
+        };
+    }
+
+    private static List<MutableComponent> getStatComponents(PokemonStats stats, String label, ChatFormatting labelColor) {
+        List<MutableComponent> components = new ArrayList<>();
+
+        components.add(Component.literal(label + ": ").withStyle(labelColor));
+
+        MutableComponent inProgress = Component.empty();
+        int currentProgress = 0;
+        int maxPerLine = 3;
+
+        for (Stats stat : Stats.values()) {
+            if (stat == Stats.ACCURACY || stat == Stats.EVASION) {
+                continue; // Skip Accuracy and Evasion stats
+            }
+
+            int statValue = stats.getOrDefault(stat);
+            ChatFormatting color = getStatColor(stat);
+            String statLabel = getStatLabel(stat);
+            inProgress.append(Component.literal("  " + statLabel + ": ").withStyle(color)
+                    .append(Component.literal(String.valueOf(statValue)).withStyle(ChatFormatting.WHITE)));
+            currentProgress++;
+
+            if (currentProgress >= maxPerLine) {
+                components.add(inProgress);
+                inProgress = Component.empty();
+                currentProgress = 0;
+            }
+        }
+
+        if (currentProgress > 0) {
+            components.add(inProgress);
+        }
+
+        return components;
+    }
+
+    private static Component[] buildComponentList(List<Component> components) {
+        return components.toArray(new Component[0]);
+    }
+
+    private static Component[] loreBuilder(Pokemon pokemon) {
+        String pokeball = pokemon.getCaughtBall().item().getDefaultInstance().getDisplayName().getString();
+        String level = String.valueOf(pokemon.getLevel());
+        String nickname = pokemon.getNickname() != null ? pokemon.getNickname().getString() : "No nickname";
+        String heldItem = pokemon.heldItem().isEmpty() ? "No held item" : pokemon.heldItem().getDisplayName().getString();
+        String OT = pokemon.getOriginalTrainerName() != null ? pokemon.getOriginalTrainerName() : "Unknown";
+        MutableComponent nature = LocalizationUtilsKt.lang(pokemon.getNature().getDisplayName().replace("cobblemon.", ""));
+        MutableComponent ability = LocalizationUtilsKt.lang(pokemon.getAbility().getDisplayName().replace("cobblemon.", ""));
+        var ivs = getStatComponents(pokemon.getIvs(), "IVs", ChatFormatting.LIGHT_PURPLE);
+        var evs = getStatComponents(pokemon.getEvs(), "EVs", ChatFormatting.DARK_AQUA);
+        var moves = getMovesComponents(pokemon);
+        String form = pokemon.getForm().getName();
+
+        List<Component> pokeLore = new ArrayList<>();
+
+        pokeLore.add(Component.literal(pokeball).setStyle(Style.EMPTY.withItalic(true)
+                .withColor(ChatFormatting.DARK_GRAY)));
+        pokeLore.add(Component.literal("Level: ").withStyle(ChatFormatting.AQUA)
+                .append(Component.literal(level).withStyle(ChatFormatting.WHITE)));
+        pokeLore.add(Component.literal("Nickname: ").withStyle(ChatFormatting.DARK_GREEN)
+                .append(Component.literal(nickname).withStyle(ChatFormatting.WHITE)));
+        pokeLore.add(Component.literal("Held Item: ").withStyle(ChatFormatting.DARK_PURPLE)
+                .append(Component.literal(heldItem).withStyle(ChatFormatting.WHITE)));
+        pokeLore.add(Component.literal("OT: ").withStyle(ChatFormatting.DARK_BLUE)
+                .append(Component.literal(OT).withStyle(ChatFormatting.WHITE)));
+        pokeLore.add(Component.literal("Nature: ").withStyle(ChatFormatting.YELLOW)
+                .append(nature.withStyle(ChatFormatting.WHITE)));
+        pokeLore.add(Component.literal("Ability: ").withStyle(ChatFormatting.GOLD)
+                .append(ability.withStyle(ChatFormatting.WHITE)));
+        pokeLore.addAll(ivs);
+        pokeLore.addAll(evs);
+        pokeLore.addAll(moves);
+        pokeLore.add(Component.literal("Form: ").withStyle(ChatFormatting.GOLD).append(Component.literal(form)));
+
+        return buildComponentList(pokeLore);
     }
 }
